617d7699949fbab70ec8a374b076578f89d73e1d
badzlan <badzlandhabith05@gmail.com>
2023-03-24 16:58:20 +0700

    model & schema

---
+++ model/User.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+   username: {
+      type: String,
+      required: [true, "Username is required!"],
+      unique: [true, "Username exist!"]
+   },
+   fullname: {
+      type: String,
+      required: [true, "Fullname is required!"],
+   },
+   city: {
+      type: String,
+      required: [true, "City is required!"],
+   },
+   country: {
+      type: String,
+      required: [true, "Country is required!"],
+   },
+   job: {
+      type: String,
+      required: [true, "Job is required!"],
+   },
+   password: {
+      type: String,
+      required: [true, "Password is required!"],
+   },
+   avatar: {
+      type: String,
+   },
+   background: {
+      type: String,
+   },
+   about: {
+      type: String,
+   },
+   instagram: {
+      type: String,
+   },
+   facebook: {
+      type: String,
+   },
+   twitter: {
+      type: String,
+   },
+})
+
+export default mongoose.model.Users || mongoose.model('User', userSchema);
