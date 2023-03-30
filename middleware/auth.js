698b66c3a0f0c80f8192df1c283765630178525b
badzlan <badzlandhabith05@gmail.com>
2023-03-30 16:34:39 +0700

    rollback

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,14 +7,10 @@ config({path: '.env'})
 const Auth = async (req, res, next) => {
    try {
       const token = req.headers.authorization.split(" ")[1];
-
       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
       req.user = decodedToken;
-
       next();
    } catch (error) {
-
       res.status(401).json({ error: "Authorization Failed!" });
    }
 }
