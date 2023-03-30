29088dd564d26c6a273a243fadab49c63c532287
badzlan <badzlandhabith05@gmail.com>
2023-03-30 17:27:23 +0700

    auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,6 @@ const Auth = async (req, res, next) => {
    try {
       const token = req.headers.authorization.split(" ")[1];
       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
       req.user = decodedToken;
       next();
    } catch (error) {
