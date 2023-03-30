b61e75a5595e3029e61241761bd8828803729fc3
badzlan <badzlandhabith05@gmail.com>
2023-03-30 17:20:13 +0700

    create api

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,6 +8,7 @@ const Auth = async (req, res, next) => {
    try {
       const token = req.headers.authorization.split(" ")[1];
       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
       req.user = decodedToken;
       next();
    } catch (error) {
