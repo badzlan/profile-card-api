a8cc7e5008536e1888c139378f9bd97902ec6263
badzlan <badzlandhabith05@gmail.com>
2023-03-30 21:58:23 +0700

    fix error again !!!

--- database/connection.js
+++ database/connection.js
@@ -1,13 +1,8 @@
 import mongoose from "mongoose";
 import { config } from "dotenv";
-
 config({path: '.env'});
 
-const connect = async () => {
-   mongoose.set('strictQuery', true);
-   const db = await mongoose.connect(process.env.DB_URL);
-   console.log("Database Connected");
-   return db;
-};
-
-export default connect;
+mongoose.connect(process.env.DB_URL);
+mongoose.connection.on('open', ()=>{
+   console.log('Database Connected');
+})
--- index.js
+++ index.js
@@ -1,34 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import connect from './database/connection.js';
-import router from './routes/route.js';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import "./database/connection.js";
+import router from "./routes/route.js";
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-app.use('/api', router);
+app.use("/api", router);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
    res.status(200).send({ msg: "Home GET Request" });
 });
 
-connect()
-   .then(() => {
-      try {
-         app.listen(port, () => {
-            console.log(`Server running on [http://127.0.0.1:${port}]. \nPress Ctrl+C to stop the server`);
-         });
-      } catch (error) {
-         console.log("Cannot connect to the server!");
-         console.log(error);
-      };
-   })
-   .catch((error) => {
-      console.log("Invalid database connection!");
-      console.log(error);
-   });
+app.listen(port, () => {
+   console.log(`Server running on [http://127.0.0.1:${port}]. \nPress Ctrl+C to stop the server`);
+});
