43b7c4486cd9c934d3b7accbf3d256f339887c7e
badzlan <badzlandhabith05@gmail.com>
2023-04-01 22:10:19 +0700

    access-control-allow-origin *

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors";
+import cors from "cors"
 import morgan from "morgan";
 import "./database/connection.js";
 import router from "./routes/route.js";
@@ -8,9 +8,7 @@ const app = express();
 const port = 3333;
 
 app.use(express.json());
-app.use(cors({
-   origin: ['http://localhost:5173', 'https://rnb-profile-card.vercel.app']
-}));
+app.use(cors());
 app.use(morgan("tiny"));
 
 app.use("/api", router);
